2053643afa96707f9a337267efd6799f670b1d28
joaoszs <joaolucio926@gmail.com>
2024-06-30 04:01:21 -0300

    adicionado novas funcionalidades

---
+++ src/main/java/list/OperacoesBasicas/CatalogoLivros.java
@@ -0,0 +1,68 @@
+package main.java.list.OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogoLivros
+{
+    private List<Livro> livroList;
+
+    public CatalogoLivros()
+    {
+        this.livroList = new ArrayList<>();
+    }
+
+    public void adicionarLivro(String titulo, String autor, int anoPublicacao)
+    {
+        livroList.add(new Livro(titulo, autor, anoPublicacao));
+    }
+
+    public List<Livro> pesquisarPorAutor(String autor)
+    {
+        List<Livro> livrosPorAutor = new ArrayList<>();
+        if (!livroList.isEmpty())
+        {
+            for (Livro L : livroList)
+            {
+                if (L.getAutor().equalsIgnoreCase(autor))
+                {
+                    livrosPorAutor.add(L);
+                }
+            }
+        }
+        return livrosPorAutor;
+    }
+
+    public List<Livro> pesquisarPorIntervaloAnos(int anoInicial, int anoFinal)
+    {
+        List<Livro> livrosPorIntervaloAnos = new ArrayList<>();
+        if (!livroList.isEmpty())
+        {
+            for (Livro l : livroList )
+            {
+                if (l.getAnoPublicacao() >= anoInicial && l.getAnoPublicacao() <= anoInicial)
+                {
+                    livrosPorIntervaloAnos.add(l);
+                }
+            }
+        }
+        return livrosPorIntervaloAnos;
+    }
+
+    public Livro pesquisarPorTitulo(String titulo)
+    {
+        Livro livroPorTitulo = null;
+        if (!livroList.isEmpty())
+        {
+            for (Livro l : livroList)
+            {
+                if (l.getTitulo().equalsIgnoreCase(titulo))
+                {
+                    livroPorTitulo = l;
+                    break;
+                }
+            }
+        }
+        return livroPorTitulo;
+    }
+}
